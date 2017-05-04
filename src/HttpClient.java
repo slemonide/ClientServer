054de3ba32ab68cf274bd1d230159bbaaf7dd43c
Danil P <slemonide@gmail.com>
2017-05-03 23:57:10 -0700

    Add a working HttpClient example.

---
+++ src/HttpClient.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Objects;
+
+/**
+ *
+ */
+public class HttpClient {
+    private static final String HOSTNAME = "dfwk.ru";
+    private static final String PAGE = "/Masterwork_dwarf_fortress";
+
+    public static void main(String[] args) throws IOException {
+        Socket sock = new Socket(HOSTNAME, 80);
+
+        BufferedReader clientReader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        PrintStream serverWriter = new PrintStream(sock.getOutputStream());
+
+        serverWriter.println("GET " + PAGE + " HTTP/1.1\r\n" +
+                "Host: " + HOSTNAME + "\r\n\r\n");
+
+        String nextLine = clientReader.readLine();
+
+        while (nextLine != null) {
+            System.out.println(clientReader.readLine());
+            nextLine = clientReader.readLine();
+        }
+    }
+}
---
+++ src/model/Protocol.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Represents a custom protocol
+ */
+public enum Protocol {
+    HEAD,
+    GET,
+    POST,
+    DONE;
+
+    Protocol() {
+        
+    }
+}
