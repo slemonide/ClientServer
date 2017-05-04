441e8c6f535a7d05480653c1cfd9c051856fb6b6
Danil P <slemonide@gmail.com>
2017-05-03 23:35:30 -0700

    Add an example that actually works.

--- src/Client.java
+++ src/Client.java
@@ -1,70 +1,24 @@
-import model.CommandInputStream;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
  * A client
  */
 public class Client {
-    // Socket leading to the server
-    private Socket serverConnection;
+    public static void main(String[] args) throws IOException {
+        String server = "127.0.0.1";
+        Socket sock = new Socket(server, 3000);
 
-    // Input command stream from the server
-    protected CommandInputStream inStream;
+        BufferedReader dis = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        PrintStream dat = new PrintStream(sock.getOutputStream());
 
-    private Client(String host, int port) throws IllegalArgumentException {
-        try {
-            System.out.println("Trying to connect to " + host + " " + port);
-            serverConnection = new Socket(host, port);
-            System.out.println("Made server connection.");
-        } catch (UnknownHostException e) {
-            throw new IllegalArgumentException("Bad host name given.");
-        } catch (IOException e) {
-            System.out.println("Client: " + e);
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
+        while (!sock.isClosed()) {
+            dat.println("PING");
 
-    public static void main(String[] args) {
-        String host = "127.0.0.1";
-        if (args.length > 0) {
-            host = args[0];
-        }
-        int port = 3000;
-        if (args.length > 1) {
-            port = Integer.parseInt(args[1]);
-        }
-
-        Client client = new Client(host, port);
-        client.sendCommands();
-    }
-
-    private void sendCommands() {
-        try {
-            OutputStreamWriter outWriter = new OutputStreamWriter(serverConnection.getOutputStream());
-            BufferedReader inReader = new BufferedReader(new InputStreamReader(serverConnection.getInputStream()));
-
-            outWriter.write("GET cat ");
-            String result = inReader.readLine();
-            System.out.println("Server says: \"" + result + "\"");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public synchronized void finalize() {
-        System.out.println("Closing down SimpleClient...");
-        try {
-            serverConnection.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(dis.readLine());
         }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -2,8 +2,7 @@ import model.CommandInputStream;
 import model.protocol.AbstractCommand;
 import model.protocol.Done;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,69 +10,23 @@ import java.net.Socket;
  * A simple server
  */
 public class Server {
-    protected int port;
-    protected ServerSocket clientConnection;
+    private static final int PORT = 3000;
 
-    public Server(int port) throws IllegalArgumentException {
-        if (port <= 0) {
-            throw new IllegalArgumentException("Bad port number.");
-        }
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
 
-        System.out.println("Connecting server socket to port...");
-        try {
-            clientConnection = new ServerSocket();
-        } catch (IOException e) {
-            System.out.println("Failed to connect to port: " + port);
-            System.exit(1);
-        }
-
-        this.port = port;
-    }
-
-    public static void main(String[] args) {
-        int port = 3000;
-        if(args.length > 0) {
-            port = Integer.parseInt(args[0]);
-        }
-
-        Server server = new Server(port);
-        System.out.println("Server running on port " + port + "...");
-        server.listen();
-    }
-
-    private void listen() {
-        System.out.println("Waiting for clients...");
         while (!Thread.interrupted()) {
-            try {
-                Socket clientRequest = clientConnection.accept();
-                System.out.println("Got a client...");
-                serviceClient(clientRequest);
-            } catch (IOException ignored) {}
+            Socket clientSocket = serverSocket.accept();
+
+            InputStream inputStream = clientSocket.getInputStream();
+            OutputStream outputStream = clientSocket.getOutputStream();
+            BufferedReader input = new BufferedReader(new InputStreamReader(inputStream));
+            PrintStream output = new PrintStream(outputStream);
+
+            System.out.println(input.readLine());
+            output.println(System.nanoTime());
+            System.out.println(System.nanoTime());
+            clientSocket.close();
         }
-    }
-
-    private void serviceClient(Socket clientRequest) {
-        try {
-            CommandInputStream inStream = new CommandInputStream(clientRequest.getInputStream());
-            DataOutputStream outputStream = new DataOutputStream(clientRequest.getOutputStream());
-
-            AbstractCommand command = null;
-            while (command == null || !(command instanceof Done)) {
-                command = inStream.readCommand();
-
-                if (command != null) {
-                    String result = command.execute();
-                    outputStream.writeBytes(result);
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public synchronized void finalize() {
-        System.out.println("Shutting down server running on port " + port);
     }
 }
